1031f149e87ab666af987dc27a86c52a144fdac9
Ashes814 <1534661514@qq.com>
2024-06-15 19:51:00 +0800

    finish version 0.0.4

---
+++ dist/assets/index-C2fEDU2i-C2fEDU2i-C2fEDU2i-C2fEDU2i-C2fEDU2i.tsx
@@ -0,0 +1,117 @@
+import ResearchProject from '@/components/ResearchProject'
+
+const Tourism = () => {
+  return (
+    <section id="tourism" className="max-w-7xl mx-auto mt-40">
+      <h2 className="text-3xl sm:text-5xl font-bold text-center mb-24">TOURIST MOBILITY & SMART TOURISM</h2>
+
+ 
+
+
+      <ResearchProject subSectionTitle="A Cross-Scale Representation of Tourist Activity Space"
+                       subSectionText="We present a conceptual framework designed to capture the spatial dynamics of tourist activities through a cross-scale perspective. The framework takes tourists’ accommodation locations as key reference points to map the organization of tourists' movements across varying spatial scales and the allocation of their time budgets. It conceptualizes a tourist’s itinerary as a combination of long-distance travels due to changes of accommodations, and localized movements within the vicinity of each accommodation location. The cross-scale approach of our framework offers a more comprehensive understanding of the spatial and temporal organization of tourist activity spaces, providing a systematic typology of tourist activity spaces and a hierarchical measurement of spatial extents and time budgets. Furthermore, this framework is adaptable to broader human movement studies that incorporate hierarchical structures, making it a versatile tool for understanding human spatial dynamics."
+                       subSectionAnno={null}
+                       subSectionImg="src/assets/images/ResearchArea/tourism/cross_scale.png"
+                       cardList={[
+                                  {title:"A Cross-Scale Representation of Tourist Activity Space",
+                                        journal:"Annals of the American Association of Geographers",
+                                        imageFront:"src/assets/images/ResearchArea/tourism/cross_scale_paper01_front.png",
+                                        imageBack:"src/assets/images/ResearchArea/tourism/cross_scale_paper01_back.png",
+                                        link:"https://www.tandfonline.com/doi/full/10.1080/24694452.2023.2231533",
+                                        isESI:false
+                                  }
+                                ]}
+                        videoList={[
+                        ]}
+                        coverCard={{
+                          coverImage:"src/assets/images/JournalCovers/AAG.jpg",
+                          link: "https://www.tandfonline.com/doi/full/10.1080/24694452.2023.2231533"
+                        }}
+                        year={"2022 - 2023"}
+                                />
+      <ResearchProject subSectionTitle="Tourist Mobility Profiling"
+                       subSectionText="In our analysis of mobile phone trajectories from international travelers visiting three distinct cities in South Korea—Jeonju, Gangneung, and Chuncheon—we have identified nine mobility indicators that encapsulate various aspects of tourist travel behavior. These indicators include the duration of stay within a city, the spatial range of activities, the number of locations visited, the number of trips conducted, and the overall mobility diversity. We scrutinize their statistical characteristics and compare them across the different cities. To delve deeper into the relationships between these mobility indicators and to discern the underlying variations in individual travel patterns, we introduce an eigendecomposition method. This technique provides a nuanced understanding of the interplay among the indicators and the unique mobility signatures of travelers."
+                       subSectionAnno={null}
+                       subSectionImg="src/assets/images/ResearchArea/tourism/tourist_mobility.png"
+                       cardList={[
+                                  {title:"Towards a multidimensional view of tourist mobility patterns in cities: A mobile phone data perspective",
+                                        journal:"Computers, Environment and Urban Systems",
+                                        imageFront:"src/assets/images/ResearchArea/tourism/tourist_mobility_paper01_front.png",
+                                        imageBack:"src/assets/images/ResearchArea/tourism/tourist_mobility_paper01_back.png",
+                                        link:"https://doi.org/10.1016/j.compenvurbsys.2020.101593",
+                                        isESI:false
+
+                                  }
+                                  
+                                ]}
+                        videoList={[
+                                 
+                        ]}
+                        coverCard={{
+                          coverImage:"src/assets/images/JournalCovers/CEUS.jpg",
+                          link: "https://doi.org/10.1016/j.compenvurbsys.2020.101593"
+                        }}
+                        year={"2020 - 2021"}
+                                />
+      <ResearchProject subSectionTitle="Understanding Tourist Time Use"
+                       subSectionText="Leveraging a comprehensive mobile phone dataset gathered from three South Korean cities—Gangneung, Jeonju, and Chuncheon—we have crafted a computational framework designed to precisely quantify tourist time utilization, visualize their spatiotemporal activity patterns, and facilitate systematic comparisons across different urban settings. This framework incorporates a variety of methodologies for extracting and semantically labeling tourist activities. It also includes visual-analytic tools, such as time use diagrams and time–activity diagrams, to investigate how tourists allocate their time. Additionally, it employs quantitative measures that enable day-to-day comparisons. This multifaceted approach offers a detailed insight into tourist behaviors, enhancing our understanding of their preferences and movements within urban environments."
+                       subSectionAnno={null}
+                       subSectionImg="src/assets/images/ResearchArea/tourism/tourist_time.webp"
+                       cardList={[
+                                  {title:"Tourism Geography through the Lens of Time Use: A Computational Framework Using Fine-Grained Mobile Phone Data",
+                                        journal:"Annals of the American Association of Geographers",
+                                        imageFront:"src/assets/images/ResearchArea/tourism/tourist_time_paper01_front.png",
+                                        imageBack:"src/assets/images/ResearchArea/tourism/tourist_time_paper01_back.png",
+                                        link:"https://www.tandfonline.com/doi/full/10.1080/24694452.2020.1812372",
+                                        isESI:false
+                                  }
+                                ]}
+                        videoList={[
+                                 
+                        ]}
+                        coverCard={{
+                          coverImage:"src/assets/images/JournalCovers/AAG.jpg",
+                          link: "https://www.tandfonline.com/doi/full/10.1080/24694452.2020.1812372"
+                        }}
+                        year={"2020 - 2021"}
+                                />
+
+<ResearchProject subSectionTitle="Tourism Network"
+                       subSectionText="Our study showcases the integration of large-scale tourist mobility data with network science methodologies to enhance our understanding of tourism destinations and their interconnectivity. Through the analysis of a mobile positioning dataset that records both the nationality and movement patterns of international tourists in South Korea, we apply several network metrics to quantify the characteristics of tourism destination networks. Our objective is to uncover the collective dynamics of tourist flows and identify distinct movement patterns among different nationalities. This approach provides valuable insights into the structural nuances of tourism networks and the diverse behaviors of travelers from various countries."
+                       subSectionAnno={null}
+                       subSectionImg="src/assets/images/ResearchArea/tourism/tourism_network.jpg"
+                       cardList={[
+                                  {title:"Characterizing destination networks through mobility traces of international tourists — A case study using a nationwide mobile positioning dataset",
+                                        journal:"Tourism Management",
+                                        imageFront:"src/assets/images/ResearchArea/tourism/tourism_network_paper01_front.png",
+                                        imageBack:"src/assets/images/JournalCovers/TM.jpg",
+                                        link:"https://doi.org/10.1016/j.tourman.2020.104195",
+                                        isESI: false
+                                  }
+                                ]}
+                        videoList={[
+                                    {
+                                    year:2020,
+                                    title:"Tourism Network",
+                                    imageFront:"src/assets/images/ResearchArea/tourism/2020_tourism_network.png",
+                                    youTubeLink:"https://www.youtube.com/watch?v=XxyWzBUp-NU",
+                                    bilibiliLink:"https://www.bilibili.com/video/BV1M5411V7ft/?vd_source=97a695383c4b98a4ed8b80d5fdecc716",
+                                    tencentLink:"https://v.qq.com/x/page/i3213kex22l.html",
+                                    type:"video",
+                                    text:""
+                                    }
+                        ]}
+                        coverCard={null}
+                        year={"2020 - 2021"}
+                                />
+
+
+      
+
+
+
+    </section>
+  )
+}
+
+export default Tourism
---
+++ dist/assets/index-DYnGzrDI-DYnGzrDI-DYnGzrDI-DYnGzrDI-DYnGzrDI-DYnGzrDI-DYnGzrDI-DYnGzrDI-DYnGzrDI.tsx
@@ -0,0 +1,91 @@
+import PredictionImg01 from '@/assets/images/ResearchArea/Prediction/prediction1.png'
+import PredictionImg02 from '@/assets/images/ResearchArea/Prediction/prediction2.png'
+import ResearchProject from '@/components/ResearchProject'
+
+
+
+
+const Prediction = () => {
+  return (
+    <section id="prediction" className="max-w-7xl mx-auto mt-40">
+      <h2 className="text-3xl sm:text-5xl font-bold text-center mb-24">MOBILITY PREDICTION & DEMAND FORECAST</h2>
+
+      <ResearchProject subSectionTitle="Demand Forecast for Bike Sharing Systems"
+                       subSectionText="Bike sharing systems have become a crucial component of the contemporary urban transportation ecosystem. In this project, we employ a data-driven approach to explore the usage patterns of these systems within cities, encompassing both dockless and station-based models, and examine how urban form and built environment characteristics impact ridership. Simultaneously, by harnessing historical travel data and environmental observations, we develop advanced deep learning algorithms aimed at providing precise and short-term forecasts of travel demand."
+                       subSectionAnno={null}
+                       subSectionImg={PredictionImg01}
+                       cardList={[
+                                  {title:"Improving short-term bike sharing demand forecast through an    irregular convolutional neural network",
+                                        journal:"Transportation Research Part C: Emerging Technologies",
+                                        imageFront:"src/assets/images/ResearchArea/Prediction/prediction_p1.png",
+                                        imageBack:"src/assets/images/JournalCovers/TRC.jpg",
+                                        link:"https://doi.org/10.1016/j.trc.2022.103984",
+                                        isESI: false
+                                  },
+                                  {title:"Short-Term Forecast of Bicycle Usage in Bike Sharing Systems: A Spatial-Temporal Memory Network",
+                                    journal:"IEEE Transactions on Intelligent Transportation Systems",
+                                    imageFront:"src/assets/images/ResearchArea/Prediction/prediction_p3.png",
+                                    imageBack:"src/assets/images/JournalCovers/ITS.png",
+                                      link:"https://ieeexplore.ieee.org/document/9497869",
+                                      isESI: false
+                                  },
+                                  {title:"Unravel the landscape and pulses of cycling activities from a dockless bike-sharing system",
+                                    journal:"Computers, Environment and Urban Systems",
+                                    imageFront:"src/assets/images/ResearchArea/Prediction/prediction_p4.png",
+                                    imageBack:"src/assets/images/JournalCovers/CEUS.jpg",
+                                    link:"https://www.sciencedirect.com/science/article/pii/S0198971518305192?dgcid=author",
+                                    isESI: true
+                                  }
+                                ]}
+                        videoList={[
+                                    {year: 2018,
+                                    title:"Shared Bikes",
+                                    imageFront:"src/assets/images/ResearchArea/Prediction/prediction_shared_bikes.png",
+                                    youTubeLink:"https://www.youtube.com/watch?v=_yfiuV4j9Jw",
+                                    bilibiliLink:"https://www.bilibili.com/video/BV1ii4y1R7of/?vd_source=97a695383c4b98a4ed8b80d5fdecc716",
+                                    tencentLink:"https://v.qq.com/x/page/a0798qotoie.html?",
+                                    type:"video",
+                                    text:""
+                                    }
+                        ]}
+                        coverCard={null}
+                        year={"2018 - Present"}
+                                />
+
+          <ResearchProject subSectionTitle="Movement Predictability of Urban Tourists"
+                       subSectionText="This study seeks to gauge the predictability of tourists' movements at the destinations. Leveraging cellphone big data from approximately 100,000 international visitors to South Korea in 2018, we construct both machine learning and deep learning models to quantify the predictability of their movements. We evaluate how factors such as duration of stay and activeness in travel patterns influence the accuracy of our predictions. Furthermore, our research uncovers the efficacy of these models in forecasting movements across various spatial dimensions, distinguishing between intra-city and inter-city travels. This nuanced understanding of traveler behavior has significant implications for enhancing tourism management and destination planning."
+                       subSectionAnno={null}
+                       subSectionImg={PredictionImg02}
+                       cardList={[
+                                  {title:"Understanding the movement predictability of international travelers using a nationwide mobile phone dataset collected in South Korea",
+                                        journal:"Computers, Environment and Urban Systems",
+                                        imageFront:"src/assets/images/ResearchArea/Prediction/prediction_p2.png",
+                                        imageBack:"src/assets/images/JournalCovers/CEUS.jpg",
+                                        link:"https://doi.org/10.1016/j.compenvurbsys.2021.101753",
+                                        isESI: false
+                                  },
+                                ]}
+                        videoList={[
+                                    {
+                                    year:2022,
+                                    title:"Urban Tour",
+                                    imageFront:"src/assets/images/ResearchArea/Prediction/prediction_urban_tour.png",
+                                    youTubeLink:"https://www.youtube.com/watch?v=_yfiuV4j9Jw",
+                                    bilibiliLink:'https://www.bilibili.com/video/BV1ii4y1R7of',
+                                    tencentLink:'',
+                                    type:"video",
+                                    text:""
+                                    }
+                        ]}
+                        coverCard={null}
+                        year={"2021 - 2022"}
+                                />
+      
+
+
+
+    </section>
+  )
+}
+
+export default Prediction
